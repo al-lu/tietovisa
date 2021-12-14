f4a91540ae3b4ffd687140e8eb3bb0b8e127ecc2
tonimatias <tonimatias96@gmail.com>
2021-12-14 14:02:11 +0200

    js tehty, tonin sivun ulkonäkö

--- js/toni.js
+++ js/toni.js
@@ -3,55 +3,70 @@
   *
 **/
 
-let counter = 0;
+
 
 function checkAnswer(){
+  let counter = 0;
   let english = document.getElementById('english');
   let lappi = document.getElementById('lappi');
   let hirvi = document.getElementById('hirvi');
   let koivu = document.getElementById('koivu');
   let vuodenaika = document.getElementById('num4');
 
-  if(english.checked === true){
+  // Ensimmäisen kysymyksen if-lauseke
+  if(english.checked){
     counter++;
     document.getElementById('questionCheck').innerHTML = "Oikein!";
+    document.getElementById('questionCheck').style.color = "green"
   }
   else {
     document.getElementById('questionCheck').innerHTML = "Pieleen meni :(";
+    document.getElementById('questionCheck').style.color = "red"
   }
-
-  if(lappi.checked ===true) {
+  // Toisen kysymyksen if-lauseke
+  if(lappi.checked) {
     counter++;
     document.getElementById('questionCheck2').innerHTML = "Oikein!";
+    document.getElementById('questionCheck2').style.color = "green"
   }
   else {
     document.getElementById('questionCheck2').innerHTML = "Pieleen meni :(";
+    document.getElementById('questionCheck2').style.color = "red"
   }
-
-  if(hirvi.checked === true) {
+  //
+  if(hirvi.checked) {
     counter++;
     document.getElementById('questionCheck3').innerHTML = "Oikein!";
+    document.getElementById('questionCheck3').style.color = "green"
   }
   else {
     document.getElementById('questionCheck3').innerHTML = "Pieleen meni :(";
+    document.getElementById('questionCheck3').style.color = "red"
   }
 
-  if(koivu.checked === true) {
+  if(koivu.checked) {
     counter++;
     document.getElementById('questionCheck4').innerHTML = "Oikein!";
+    document.getElementById('questionCheck4').style.color = "green"
   }
   else {
     document.getElementById('questionCheck4').innerHTML = "Pieleen meni :(";
+    document.getElementById('questionCheck4').style.color = "red"
   }
 
-  if(vuodenaika.checked === true) {
+  if(vuodenaika.checked) {
     counter++;
     document.getElementById('questionCheck5').innerHTML = "Oikein!";
+    document.getElementById('questionCheck5').style.color = "green"
   }
   else {
     document.getElementById('questionCheck5').innerHTML = "Pieleen meni :(";
+    document.getElementById('questionCheck5').style.color = "red"
   }
 
-  document.getElementById('total').innerHTML = "Sait " + counter + " pistettä! " + "Maksimi pistemäärä on 5."
+  document.getElementById('total').innerHTML = "Pisteesi: " + counter + ". " + "Maksimi pistemäärä on 5."
+
+  document.getElementById('check').style.display = "none"
+  document.getElementById('refresh').style.display = "inline-block"
 }
 
