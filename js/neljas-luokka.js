7771e3fa34d8111050f0c00d73210c79ae2ac970
SamiNousiainen <n1nosa00@students.oamk.fi>
2021-12-15 16:56:27 +0200

    4th grade js comments 15122021

--- js/neljas-luokka.js
+++ js/neljas-luokka.js
@@ -4,21 +4,27 @@
   *
 */
 
+
+//funktio vastausten tarkistamiseen
 function checkAnswer() {
 
+  //muuttuja pistelaskurille
   let counter = 0
 
-// kys1
+//kysymys 1 tarkistaminen
   if (document.getElementById("true1").checked) {
+    //jos oikein, näytä teksti vihreänä
     document.getElementById("answer1").innerHTML = "Oikein"
     document.getElementById("answer1").style.color = "green"
+    //+1 piste
     counter++
   } else {
+    //jos väärin, näytä teksti punaisena
     document.getElementById("answer1").innerHTML = "Väärin, EU maita on 27."
     document.getElementById("answer1").style.color = "red"
   }
 
-  //kys2
+  //kysymys 2
   if (document.getElementById("true2").checked) {
     document.getElementById("answer2").innerHTML = "Oikein"
     document.getElementById("answer2").style.color = "green"
@@ -28,7 +34,7 @@ function checkAnswer() {
     document.getElementById("answer2").style.color = "red"
   }
 
-  //kys3
+  //kysymys 3
   if (document.getElementById("true3").checked) {
     document.getElementById("answer3").innerHTML = "Oikein"
     document.getElementById("answer3").style.color = "green"
@@ -38,7 +44,7 @@ function checkAnswer() {
     document.getElementById("answer3").style.color = "red"
   }
 
-  //kys4
+  //kysymys 4
   if (document.getElementById("true4").checked) {
     document.getElementById("answer4").innerHTML = "Oikein"
     document.getElementById("answer4").style.color = "green"
@@ -48,7 +54,7 @@ function checkAnswer() {
     document.getElementById("answer4").style.color = "red"
   }
 
-  //kys5
+  //kysymys 5
   if (document.getElementById("true5").checked) {
     document.getElementById("answer5").innerHTML = "Oikein"
     document.getElementById("answer5").style.color = "green"
@@ -58,8 +64,10 @@ function checkAnswer() {
     document.getElementById("answer5").style.color = "red"
   }
 
+  //näytetään tarkistaessa pistelaskuri
   document.getElementById("points").innerHTML = "Pisteet: " + counter + "/5"
 
+  //piilotetaan tarkistusnappi ja näytetään sivun päivitysnappi
   document.getElementById("button").style.display = "none"
   document.getElementById("refresh").style.display = "inline-block"
 }
