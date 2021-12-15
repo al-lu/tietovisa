04bb109e862c1349b7bb2d0b42832a83e97e13f2
JSKarhu <92786584+JSKarhu@users.noreply.github.com>
2021-12-15 16:55:07 +0200

    lisätty kommentteja

--- js/ensimmainen-luokka.js
+++ js/ensimmainen-luokka.js
@@ -1,22 +1,18 @@
-
-/**
-  * @author Joona Karhu
-  *
-=======
 /**
   * @author Joona Karhu
   *
 
 */
 
-// let check = document.getElementById('check').innerHTML;
+// created a variable for correct and wrong answers
 let correctAns = 'Oikea vastaus! :)'
 let wrongAns = 'Nyt ei mennyt ihan oikein :('
 
-
+// function for checking the answers, and with check-button 
+// Luodaan funktio, missä tarkistetaan vastaukset, ja check-nappia painaessa kerrotaan ovatko vastaukset oikein. Jos ovat, niin counteria kasvatetaan
 function checkAnswers() {
   let counter = 0;
-    // tarkistetaan ensimmäisen kysymyksen vastaus
+    // Tarkistetaan ensimmäisen kysymyksen vastaus
     if (document.getElementById('green').checked) {
       document.getElementById('answer1').innerHTML = correctAns;
       document.getElementById('answer1').style.color = 'green';
@@ -26,7 +22,7 @@ function checkAnswers() {
       document.getElementById('answer1').innerHTML = wrongAns;
       document.getElementById('answer1').style.color = 'red';
     }
-    // tarkistetaan toisen kysymyksen vastaus
+    // Tarkistetaan toisen kysymyksen vastaus
     if (document.getElementById('ruotsi').checked) {
       document.getElementById('answer2').innerHTML = correctAns;
       document.getElementById('answer2').style.color = 'green';
@@ -36,7 +32,7 @@ function checkAnswers() {
       document.getElementById('answer2').innerHTML = wrongAns;
       document.getElementById('answer2').style.color = 'red';
     }
-    // tarkistetaan kolmannen kysymyksen vastaus
+    // Tarkistetaan kolmannen kysymyksen vastaus
     if (document.getElementById('joutsen').checked) {
       document.getElementById('answer3').innerHTML = correctAns;
       document.getElementById('answer3').style.color = 'green';
@@ -46,7 +42,7 @@ function checkAnswers() {
       document.getElementById('answer3').innerHTML = wrongAns;
       document.getElementById('answer3').style.color = 'red';
     }
-    // tarkistetaan neljännen kysymyksen vastaus
+    // Tarkistetaan neljännen kysymyksen vastaus
     if (document.getElementById('helsinki').checked) {
       document.getElementById('answer4').innerHTML = correctAns;
       document.getElementById('answer4').style.color = 'green';
@@ -56,7 +52,7 @@ function checkAnswers() {
       document.getElementById('answer4').innerHTML = wrongAns;
       document.getElementById('answer4').style.color = 'red';
     }
-    // tarkistetaan viidennen kysymyksen vastaus
+    // Tarkistetaan viidennen kysymyksen vastaus
     if (document.getElementById('kaksitoista').checked) {
       document.getElementById('answer5').innerHTML = correctAns;
       document.getElementById('answer5').style.color = 'green';
@@ -66,6 +62,8 @@ function checkAnswers() {
       document.getElementById('answer5').innerHTML = wrongAns;
       document.getElementById('answer5').style.color = 'red';
     }
+
+    // Nappia painaessa lasketaan ja tulostaan pistemäärä, piilotetaan check-nappi ja tuodaan reload-nappi näkyviin 
     document.getElementById('points').innerHTML = 'Pistemääräsi on: ' + counter + '/5';
     document.getElementById('check').style.display = 'none';
     document.getElementById('reload').style.display = 'block';
